f24b06d2a6a6bbe8f5e68ae2da9bd2c949e91be5
sara <20233005742@estudantes.ifpr.edu.br>
2025-05-08 08:10:55 -0300

    Adicionei Outro Template

--- js/custom.js
+++ js/custom.js
@@ -1,92 +1,72 @@
-
-
-// ISOTOPE FILTER
-
-jQuery(document).ready(function($){
-
-	if ( $('.iso-box-wrapper').length > 0 ) { 
-
-	    var $container 	= $('.iso-box-wrapper'), 
-	    	$imgs 		= $('.iso-box img');
-
-
-
-	    $container.imagesLoaded(function () {
-
-	    	$container.isotope({
-				layoutMode: 'fitRows',
-				itemSelector: '.iso-box'
-	    	});
-
-	    	$imgs.load(function(){
-	    		$container.isotope('reLayout');
-	    	})
-
-	    });
-
-	    //filter items on button click
-
-	    $('.filter-wrapper li a').click(function(){
-
-	        var $this = $(this), filterValue = $this.attr('data-filter');
-
-			$container.isotope({ 
-				filter: filterValue,
-				animationOptions: { 
-				    duration: 750, 
-				    easing: 'linear', 
-				    queue: false, 
-				}              	 
-			});	            
-
-			// don't proceed if already selected 
-
-			if ( $this.hasClass('selected') ) { 
-				return false; 
-			}
-
-			var filter_wrapper = $this.closest('.filter-wrapper');
-			filter_wrapper.find('.selected').removeClass('selected');
-			$this.addClass('selected');
-
-	      return false;
-	    }); 
-
-	}
-
+// ------- PRELOADER -------//
+$(window).load(function(){
+    $('.preloader').fadeOut("slow"); // set duration in brackets    
 });
+// ----- GOOGLE MAP ----- //
+var map = '';
+var center;
 
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
-// MAIN NAVIGATION
-
- $('.main-navigation').onePageNav({
-        scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
-        scrollOffset: 75, //Height of Navigation Bar
-        filter: ':not(.external)',
-        changeHash: true
-    }); 
-
-    /* NAVIGATION VISIBLE ON SCROLL */
-    mainNav();
-    $(window).scroll(function () {
-        mainNav();
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
     });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
 
-    function mainNav() {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({
-            "opacity": '1',
-            "top": '0'
-        });
-        else $('.sticky-navigation').stop().animate({
-            "opacity": '0',
-            "top": '-75'
-        });
-    }
+function calculateCenter() {
+  center = map.getCenter();
+}
 
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
 
-// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(function(){
 
+   // --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
+
+  // --------- PORTFOLIO IMAGE ----- //
+  $('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+  // ------- WOW ANIMATED ------ //
+  wow = new WOW(
+  {
+    mobile: false
+  });
+  wow.init();
+
+  // ------- GOOGLE MAP ----- //
+  loadGoogleMap();
+
+  // ------- JQUERY PARALLAX ---- //
+  function initParallax() {
+    $('#home').parallax("100%", 0.3);
+    $('#team').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.1);
+
+  }
+  initParallax();
+
+});
+
